e714f588f3e8016bbc660c69e6ce29a444a8fddc
Ali Can Batur <alicanbatur@gmail.com>
2017-12-14 13:51:46 +0300

    Initial commit

---
+++ Example/ABExpandableView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ABExpandableView
+//
+//  Created by alicanbatur on 12/14/2017.
+//  Copyright (c) 2017 alicanbatur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
